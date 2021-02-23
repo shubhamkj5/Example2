1215a7bdb4138fe8a539b399e147437ab1b50a39
shubhamkj5 <shubhamkj2405@gmail.com>
2021-02-23 19:21:03 +0000

    updated m3

--- module3.py
+++ module3.py
@@ -1,3 +1,14 @@
-kjfg
-dfgkjdf
-dfgjh
+savings = 100
+growth_multiplier = 1.1
+desc = "compound interest"
+
+# Assign product of growth_multiplier and savings to year1
+
+
+# Print the type of year1
+
+
+# Assign sum of desc and desc to doubledesc
+
+
+# Print out doubledesc
